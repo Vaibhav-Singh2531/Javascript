a5b8607a82c06b3da0fe325ca98c8d4e84ecf079
Vaibhav-Singh2531 <vaibhavsingh2531@gmail.com>
2026-06-09 22:10:50 +0530

    Let vs Var vs Const

---
+++ Let vs Var vs Const/app.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"     // same as var accountCity = "Jaipur"
+let accountState;  // undefined
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.log(accountState);
+
